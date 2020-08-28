4e33dd4ee86212f063b338f2a8a7a55971596c1f
Shadow <anucha.pinsaimoon@gmail.com>
2020-08-28 16:05:32 +0700

    put array data

--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import numpy as np
+import random
 from numpy import genfromtxt
+my_data = np.genfromtxt('data.csv', dtype=int, delimiter='\t', names=True)
+data = [i for i in np.array(my_data)]
 
 if __name__ == "__main__":
-    data = np.genfromtxt('data.csv', dtype=None, delimiter='\t', names=True)
-    print(data)
+
+    for i in range(len(data)):
+        for j in range(8):
+            print(data[i][j],end='   ')
+        print('\n')
     pass
